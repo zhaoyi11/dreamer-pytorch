Fix incorrect dimensions in discrete setting.

agent.py
@@ -81,11 +81,12 @@ class Dreamer():
             args.cnn_act).to(device=args.device)
 
     self.actor_model = ActorModel(
-            args.action_size,
+            args.action_size if not args.discrete else args.actions_n,
             args.belief_size,
             args.state_size,
             args.hidden_size,
-            activation_function=args.dense_act).to(device=args.device)
+            activation_function=args.dense_act,
+            discrete=args.discrete).to(device=args.device)
 
     self.value_model = ValueModel(
             args.belief_size,
@@ -331,12 +332,21 @@ class Dreamer():
 
     return belief, posterior_state
 
+  def add_noise(self, action):
+    if self.args.discrete:
+      # TODO: decay exploration amount
+      return torch.where(torch.rand(action.size(0)) < self.args.expl_amount,
+                         torch.randint(0, self.args.actions_n, action.size(), dtype=action.dtype),
+                         action)
+    else:
+      action = Normal(action, self.args.expl_amount).rsample()
+      return torch.clamp(action, -1, 1)
+
   def select_action(self, state, deterministic=False):
     # get action with the inputs get from fn: infer_state; return a numpy with shape [batch, act_size]
     belief, posterior_state = state
     action, _ = self.actor_model(belief, posterior_state, deterministic=deterministic, with_logprob=False)
 
     if not deterministic and not self.args.with_logprob: ## add exploration noise
-      action = Normal(action, self.args.expl_amount).rsample()
-      action = torch.clamp(action, -1, 1)
+      action = self.add_noise(action)
     return action  # tensor

models.py
@@ -323,7 +323,6 @@ class OneHotDist:
     def __init__(self, logits=None, probs=None):
         self._dist = torch.distributions.Categorical(logits=logits, probs=probs)
         self._num_classes = logits.size(-1) # TODO: DIFF
-        self._dtype = torch.int # TODO: fixed for now
 
     @property
     def name(self):
@@ -342,11 +341,10 @@ class OneHotDist:
     def mode(self):
         return self._dist.probs.argmax(-1) # TODO: mode doesn't exist in torch
 
-    def rsample(self, amount=None):
+    def rsample(self):
         # Does gym (or whatever environment engine) expect one-hot encoded actions?
-        amount = [amount] if amount else []
-        samples = self._dist.sample(*amount)
+        samples = self._dist.sample().type(torch.float32)
         #samples = self._one_hot(samples)
-        probs = self._dist.probs[samples]
-        samples += (probs - probs.detach()).type(self._dtype)
-        return samples
+        probs = self._dist.log_prob(samples)
+        samples += (probs - probs.detach())
+        return samples.unsqueeze(-1)